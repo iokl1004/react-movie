30da74706bcc6aeb70bd736de3c22fe2b138321d
iokl1004 <iokl3319@naver.com>
2022-02-15 15:24:39 +0900

    upload

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,12 +1,11 @@
-import React from "react";
 import styles from "./Loading.module.css";
 
 function Loading() {
     return (
-        <div>
-            <img className={styles.loading} src="/Loading.gif" />
+        <div className={styles.loader}>
+          <strong>Loading...</strong>
         </div>
-    );
+      )
 }
 
 export default Loading;
